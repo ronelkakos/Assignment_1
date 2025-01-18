aebd55137ce3607423e45bad5de3a78f4d9caae4
ronelkakos <ronelk408@gmail.com>
2025-01-18 18:25:24 -0500

    Updated my Assignment1 with adding the difference calculation

--- Assignment1.py
+++ Assignment1.py
@@ -3,3 +3,5 @@
 a = int(input("Enter the first number to add: "))
 b = int(input("Enter the second number to add: "))
 print(f"The sum is {a + b}")
+# Updated my code to add the difference calculation
+print(f"The difference is {a - b}")
